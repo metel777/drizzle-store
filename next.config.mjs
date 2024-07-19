cb5c235700524b57b59e66e536db303d2d4f83aa
Metel <creatorelate@gmail.com>
2024-07-19 14:18:19 +0300

    argon fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-//   experimental: {
-//   	serverComponentsExternalPackages: ["@node-rs/argon2"]
-//   }
   experimental: {
+  	serverComponentsExternalPackages: ["@node-rs/argon2"],
     outputFileTracingIncludes: {
       "/": ["./node_modules/argon2/prebuilds/linux-x64/*.musl.*"],
     },
-  },
+  }
 }
 
 export default nextConfig
